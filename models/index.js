8a4122f4faead3b561b43e226f037a7cdcf12310
Tyler Farrior <tylerfarrior@gmail.com>
2023-09-26 16:24:51 -0400

    set up model associations

--- models/index.js
+++ models/index.js
@@ -5,12 +5,34 @@ const Tag = require('../Tag');
 const ProductTag = require('../ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+
+// Products hasMany Tags
+Product.hasMany(Tag, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  onDelete: 'CASCADE',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  onDelete: 'CASCADE',
+});
 
 module.exports = {
   Product,
